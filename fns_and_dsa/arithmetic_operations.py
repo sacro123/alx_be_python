9f99c98ea8473bcd494015c499ba09ef6e85850b
sacro123 <sabrinataoussi@gmail.com>
2024-07-04 03:19:12 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -21,7 +21,7 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         result = num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
+    if num2 != 0:
             result = num1 / num2
         else:
             result = "Error: Division by zero"
